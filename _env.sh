0f82d19be57c2a0e339d5155e3209dbf1894afea
Jean Guillaume <jgi@jgwill.com>
2022-10-01 11:34:43 -0400

    env

---
+++ _env.sh
@@ -0,0 +1,38 @@
+dockertag=jgwill/dotnet:6.0
+
+containername=jgwilldotnet
+dkhostname=$containername
+
+# PORT
+#dkport=4000:4000
+
+# VOLUMES
+#dkvolume="myvolname220413:/app" #create or use existing one
+#dkvolume="$containername:/app" #create with containername name
+
+#dkecho=true #just echo the docker run
+
+dkcommand=bash #command to execute (default is the one in the dockerfile)
+
+#dkextra="MY EXTRA ARGS"
+
+#dkmounthome=true
+
+#xmount=/tmp:/a/tmp
+#xmount2=/var:/a/var
+
+# Use TZ
+#DK_TZ=1
+
+# RUN MODE
+#dkrunmode="bg" #default fg
+#dkrestart="--restart" #default
+#dkrestarttype="unless-stopped" #default
+
+
+#####################################
+#Build related
+#
+##chg back to that user
+#dkchguser=vscode
+
